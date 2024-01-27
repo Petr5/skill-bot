ef48011188596bf31ebdde5406f0e3afda3276b5
pvmavzovin <pvmavzovin@edu.hse.ru>
2024-01-27 21:33:31 +0300

    add downloading specific post(problem with blocking them)

--- skills_bot/telegram-bot/bot.py
+++ skills_bot/telegram-bot/bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 import sys
 from os import getenv
 
@@ -8,8 +9,10 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message, FSInputFile
 from aiogram.utils.markdown import hbold
+from aiogram.utils.media_group import MediaGroupBuilder
 from instaloader import instaloader
 from PIL import Image
+import re
 from aiogram.methods.send_photo import SendPhoto
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = getenv("BOT_TOKEN")
@@ -39,7 +42,7 @@ def sanitize_filename(filename):
     return "".join(c if c.isalnum() or c in "_-" else "_" for c in filename)
 
 @dp.message(lambda message: message.text.startswith("/download_post")) # TODO need to download random post by link
-async def download_post_handler(message: types.Message) -> None:
+async def download_profile_post(message: types.Message) -> None:
     try:
         # Extract the Instagram account name from the command text
         command_args = message.text.split()
@@ -77,6 +80,39 @@ async def download_post_handler(message: types.Message) -> None:
 
     except Exception as e:
         await message.reply(f"An error occurred: {e}")
+
+instagram_post_pattern = r'https://www\.instagram\.com/p/([A-Za-z0-9_-]+)/'
+
+@dp.message(lambda message: re.match(instagram_post_pattern, message.text)) # TODO blocking most requests as spam(JSON Query to graphql/query: HTTP error code 401.)
+async def download_post(message: types.Message) -> None:
+    try:
+        # Extract the post code from the URL
+        match = re.match(instagram_post_pattern, message.text)
+        if not match:
+            await message.reply("Invalid Instagram URL format.")
+            return
+
+        post_code = match.group(1)
+        print("post_code ", post_code)
+        # Initialize instaloader
+        loader = instaloader.Instaloader()
+        post = instaloader.Post.from_shortcode(loader.context, post_code)
+
+        # Download the post
+        dirname = fr'{post.owner_username}\{post_code}'
+        if loader.download_post(post, dirname):
+            media_group = MediaGroupBuilder(caption="Media group caption")
+
+            for file_name in os.listdir(dirname):
+                file_path = f'{dirname}{file_name}'
+                media_group.add_photo(FSInputFile(file_path))
+
+            await bot.send_media_group(chat_id=message.chat.id, media=media_group.build())
+        else:
+            print("L.download_post return false")
+
+    except Exception as e:
+        await message.reply(f"An error occurred while downloading the post: {str(e)}")
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
