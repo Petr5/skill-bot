34cb56778ace9c9444de8058c7b588b4fc4ce725
pvmavzovin <pvmavzovin@edu.hse.ru>
2024-01-27 18:00:17 +0300

    add downloading last post from insta profile, and send them photo back

--- skills_bot/database/models.py
+++ skills_bot/database/models.py
@@ -1,19 +1,107 @@
-from sqlalchemy import create_engine, Column, Integer, String, Sequence
-from sqlalchemy.dialects.mysql import aiomysql
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from __future__ import annotations
+
+import asyncio
+import datetime
+from typing import List
+
+from sqlalchemy import ForeignKey
+from sqlalchemy import func
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import selectinload
 from DB_config import DATABASE_URL
-Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
 
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    username = Column(String(50), unique=True)
 
-# Replace 'DATABASE_URL' with your database URL
-engine = create_engine(DATABASE_URL, echo=True)
+class A(Base):
+    __tablename__ = "a"
 
-Base.metadata.create_all(bind=engine)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    data: Mapped[str]
+    create_date: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
+    bs: Mapped[List[B]] = relationship()
 
-AsyncSession = sessionmaker(bind=engine, class_=aiomysql.sa.orm.Session, expire_on_commit=False)
+
+class B(Base):
+    __tablename__ = "b"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    a_id: Mapped[int] = mapped_column(ForeignKey("a.id"))
+    data: Mapped[str]
+
+
+async def insert_objects(async_session: async_sessionmaker[AsyncSession]) -> None:
+    async with async_session() as session:
+        async with session.begin():
+            session.add_all(
+                [
+                    A(bs=[B(data="b1"), B(data="b2")], data="a1"),
+                    A(bs=[], data="a2"),
+                    A(bs=[B(data="b3"), B(data="b4")], data="a3"),
+                ]
+            )
+
+
+
+async def select_and_update_objects(
+    async_session: async_sessionmaker[AsyncSession],
+) -> None:
+    async with async_session() as session:
+        stmt = select(A).options(selectinload(A.bs))
+
+        result = await session.execute(stmt)
+
+        for a1 in result.scalars():
+            print(a1)
+            print(f"created at: {a1.create_date}")
+            for b1 in a1.bs:
+                print(b1)
+
+        result = await session.execute(select(A).order_by(A.id).limit(1))
+
+        a1 = result.scalars().one()
+
+        a1.data = "new data"
+
+        await session.commit()
+
+        # access attribute subsequent to commit; this is what
+        # expire_on_commit=False allows
+        print(a1.data)
+
+        # alternatively, AsyncAttrs may be used to access any attribute
+        # as an awaitable (new in 2.0.13)
+        for b1 in await a1.awaitable_attrs.bs:
+            print(b1)
+
+
+async def async_main() -> None:
+    engine = create_async_engine(
+        DATABASE_URL,
+        echo=True,
+    )
+
+    # async_sessionmaker: a factory for new AsyncSession objects.
+    # expire_on_commit - don't expire objects after transaction commit
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    await insert_objects(async_session)
+    await select_and_update_objects(async_session)
+
+    # for AsyncEngine created in function scope, close and
+    # clean-up pooled connections
+    await engine.dispose()
+
+
+asyncio.run(async_main())
--- skills_bot/telegram-bot/bot.py
+++ skills_bot/telegram-bot/bot.py
@@ -6,14 +6,18 @@ from os import getenv
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from aiogram.utils.markdown import hbold
-
+from instaloader import instaloader
+from PIL import Image
+from aiogram.methods.send_photo import SendPhoto
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = getenv("BOT_TOKEN")
-
+INSTAGRAM_ACCOUNT = "petr.mavzovin"
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
+bot = Bot(token=TOKEN)
+
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
@@ -28,21 +32,51 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
 
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
+
+def sanitize_filename(filename):
+    # Заменяем недопустимые символы в имени файла
+    return "".join(c if c.isalnum() or c in "_-" else "_" for c in filename)
+
+@dp.message(lambda message: message.text.startswith("/download_post")) # TODO need to download random post by link
+async def download_post_handler(message: types.Message) -> None:
     try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+        # Extract the Instagram account name from the command text
+        command_args = message.text.split()
+        if len(command_args) < 2:
+            await message.reply("Please specify the Instagram account name after the command.")
+            return
 
+        instagram_account = command_args[1]
 
+        # Initialize instaloader
+        L = instaloader.Instaloader()
+
+        # Download the profile info
+        profile = instaloader.Profile.from_username(L.context, instagram_account)
+
+        # Download the most recent post
+        for post in profile.get_posts():
+            # Download the post
+            L.download_post(post, target=instagram_account)
+
+            # Format the date and time
+            formatted_datetime = post.date_utc.strftime("%Y-%m-%d_%H-%M-%S_UTC")
+
+            # Create the file path
+            file_path = f"{instagram_account}/{formatted_datetime}.jpg"
+            file = open(file_path)
+            image = Image.open(file_path)
+
+            # Display the image
+            image.show()
+
+            await bot.send_photo(chat_id=message.from_user.id, photo=FSInputFile(file_path))
+
+            break  # Exit the loop after the first post
+
+    except Exception as e:
+        await message.reply(f"An error occurred: {e}")
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
